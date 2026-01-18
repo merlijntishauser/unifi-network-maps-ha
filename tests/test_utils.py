1d45b902912919d1be8c3217c9b1c3f9a753a094
Merlijn Tishauser <merlijn@ccadd.nl>
2026-01-18 13:20:06 +0100

    Remove old test paths after reorg

--- tests/test_utils.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from custom_components.unifi_network_map import utils
-
-
-def test_monotonic_seconds_returns_float() -> None:
-    result = utils.monotonic_seconds()
-
-    assert isinstance(result, float)
