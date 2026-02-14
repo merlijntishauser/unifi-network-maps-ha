297a8abf6019e1b6f0d136159bf178f0978685ba
Merlijn Tishauser <merlijn@ccadd.nl>
2026-02-14 01:06:52 +0100

    Bump version to 0.3.3

--- custom_components/unifi_network_map/frontend/unifi-network-map.js
+++ custom_components/unifi_network_map/frontend/unifi-network-map.js
@@ -7956,7 +7956,7 @@ var UnifiNetworkMapEditor = class extends HTMLElement {
 };
 
 // src/unifi-network-map.ts
-var CARD_VERSION = "0.3.2";
+var CARD_VERSION = "0.3.3";
 customElements.define("unifi-network-map", UnifiNetworkMapCard);
 customElements.define("unifi-network-map-editor", UnifiNetworkMapEditor);
 window.customCards = window.customCards || [];
